9122f86ab9d25702bbba19f08e74e39232cd18de
Arya <rvarya888@gmail.com>
2025-05-06 00:12:37 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
   const audioRef = useRef(null)
 
   // For testing
-  // const birthdayDate = new Date("2025-05-06T00:00:00")
+  // const birthdayDate = new Date("2025-05-06T00:20:00")
 
   useEffect(() => {
     setTimeout(() => {
